ba2708ab62eacd1f5b7133ee5b357b1a6a4dec85
AliyaBest <aliyabest@gmail.com>
2020-06-13 08:55:13 -0400

    called done()

--- server/index.test.js
+++ server/index.test.js
@@ -5,16 +5,17 @@ const db = require('./db')
 const seedUsers = require('../script/users.json')
 
 
-describe('GET /users', function () {
+describe('GET /users', () => {
 
-  before(async function () {
+  before(async () => {
     await db.sync({ force: true })
     await db.models.user.bulkCreate(seedUsers)
   })
 
-  it('should return list of users', async function () {
+  it('should return list of users', async (done) => {
     const res = await request(app).get('/api/users')
     expect(res.status).to.equal(200)
     expect(res.body.length).to.equal(seedUsers.length)
+    done()
   })
 })
